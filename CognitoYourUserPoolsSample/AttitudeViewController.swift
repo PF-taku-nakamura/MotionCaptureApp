5edadf935d6a256f05c95315bcaebaefb19f9ed6
nakamura-taku <t_nakamura@ynl.t.u-tokyo.ac.jp>
2018-12-10 22:34:07 +0900

    Create view transition button

---
+++ CognitoYourUserPoolsSample/AttitudeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AttitudeViewController.swift
+//  CognitoYourUserPoolsSample
+//
+//  Created by 中村拓 on 2018/12/10.
+//  Copyright © 2018年 Dubal, Rohan. All rights reserved.
+//
+
+import UIKit
+
+class AttitudeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
