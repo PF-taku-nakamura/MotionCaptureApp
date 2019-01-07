3e5ff3ab239bafc94b67f4866207f675d95ab333
nakamura-taku <t_nakamura@ynl.t.u-tokyo.ac.jp>
2019-01-07 19:03:38 +0900

    I set that the app does not sleep automatically.

--- CognitoYourUserPoolsSample/AppDelegate.swift
+++ CognitoYourUserPoolsSample/AppDelegate.swift
@@ -36,6 +36,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        // アプリが勝手にスリープモードになるのを防ぐ
+        UIApplication.shared.isIdleTimerDisabled = true
+        
         // ユーザーからPush Nortification通知の許可をもらう.
         if #available(iOS 10.0, *) {
             // iOS 10 以降の設定
